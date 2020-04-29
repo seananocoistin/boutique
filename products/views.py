5c871a13e51f1bf90da88939b1fd97917ee11cf4
Seanán Ó Coistín <seananoc@gmail.com>
2020-04-29 16:44:45 +0000

    Added product details functionality

--- products/views.py
+++ products/views.py
@@ -13,10 +13,10 @@ def all_products(request):
 
 def product_detail(request, product_id):
 
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
